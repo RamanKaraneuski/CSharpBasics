ec8ac44fe7af7475574a05d03f0d37a6afdb9bfe
RamanKaraneuski <raman.karaneuski@gmail.com>
2023-06-21 21:36:43 +0300

    Add Homework CSharpOOP3

---
+++ CSharpOOP3/Horse.cs
@@ -0,0 +1,30 @@
+﻿public class Horse
+{
+    private bool isCompetitionReady;
+    private double speed;
+    private string name;
+
+    public bool IsCompetitionReady
+    {
+        get { return isCompetitionReady; }
+    }
+
+    public double Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public static string Kind { get; set; } = "Horse";
+
+    public static void ShowMessage()
+    {
+        Console.WriteLine("Hello from static method!");
+    }
+}
